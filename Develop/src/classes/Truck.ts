23a9628bf16700055eda8fa91657fca6655101ec
Kevin Perez <kevin_perez14@comcast.net>
2024-11-25 00:15:02 -0500

    Initial commit

---
+++ Develop/src/classes/Truck.ts
@@ -0,0 +1,70 @@
+// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+import Vehicle from './Vehicle.js';
+import Motorbike from './Motorbike.js';
+import Car from './Car.js';
+import Wheel from './Wheel.js';
+import AbleToTow from '../interfaces/AbleToTow.js';
+
+class Truck extends Vehicle {
+
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+  towingCapacity: number;
+
+  constructor(
+    vin: string,
+    color: string,
+    make: string,
+    model: string,
+    year: number,
+    weight: number,
+    topSpeed: number,
+    wheels: Wheel[],
+    towingCapacity: number
+  ) {
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+    this.towingCapacity = towingCapacity;
+  }
+
+  tow(vehicle: Car | Motorbike): void {
+    const vehicleDescription = `${vehicle.make} ${vehicle.model}`;
+    
+    if (vehicle.weight <= this.towingCapacity) {
+      console.log(`The ${vehicleDescription} is being towed by the truck.`);
+    } else {
+      console.log(`The ${vehicleDescription} is too heavy to be towed by this truck.`);
+    }
+  }
+
+  override printDetails(): void {
+    super.printDetails();
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Towing Capacity: ${this.towingCapacity} lbs`);
+    console.log('Wheels:');
+    this.wheels.forEach((wheel, index) => {
+      console.log(`  Wheel ${index + 1}: ${wheel.getTireBrand} - ${wheel.getDiameter} inches`);
+    });
+  }
+}
+
+export default Truck;
